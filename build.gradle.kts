c50231b90fdf1e5794e8c177cc4684f1a0bfda96
Lucas <lucasvalenteds@gmail.com>
2022-04-23 19:59:37 -0300

    Adding project dependencies
    Spring Context, Jackson, Kinesis Client Library and Mockito.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,12 @@ dependencies {
     implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
     implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
 
+    implementation("org.springframework", "spring-context", properties["version.spring"].toString())
+    implementation("com.fasterxml.jackson.core", "jackson-databind", properties["version.jackson"].toString())
+    implementation("software.amazon.kinesis", "amazon-kinesis-client", properties["version.kcl"].toString())
+
     testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
+    testImplementation("org.mockito", "mockito-core", properties["version.mockito"].toString())
 }
 
 java {
