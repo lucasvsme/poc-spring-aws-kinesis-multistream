6ddfbe7726405e70428815a697d2ebbda555ae5e
Lucas <lucasvalenteds@gmail.com>
2022-04-24 22:06:38 -0300

    Implementing StreamException
    It inherits from EventException, which is an abstract class that extends
    from RuntimeException, and contains multiple constructors provideing
    fine grained error handling.

---
+++ src/main/java/com/example/streaming/configuration/StreamException.java
@@ -0,0 +1,30 @@
+package com.example.streaming.configuration;
+
+import com.example.event.EventException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import software.amazon.awssdk.core.exception.SdkException;
+
+import java.io.IOException;
+import java.io.Serial;
+
+public final class StreamException extends EventException {
+
+    @Serial
+    private static final long serialVersionUID = -4146245509402692451L;
+
+    public StreamException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    public StreamException(SdkException exception) {
+        super("Error publishing event to stream", exception);
+    }
+
+    public StreamException(JsonProcessingException exception) {
+        super("Error serializing event to JSON", exception);
+    }
+
+    public StreamException(IOException exception) {
+        super("Error deserializing event to JSON", exception);
+    }
+}
