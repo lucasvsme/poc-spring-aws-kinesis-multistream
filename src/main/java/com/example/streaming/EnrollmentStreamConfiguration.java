3eb4688210d0b1fa16da17dc9d990f1448263ca2
Lucas <lucasvalenteds@gmail.com>
2022-05-03 00:45:23 -0300

    Enabling stream consumption
    Creating Configuration classes to expose schedulers and processors for
    each group of events. The configuration is based on values defined in
    property file with fallback from the environment.
    
    Each configuration class provides one instance of StreamProperties that
    contains the information about the stream that should be consumed.
    
    They also provide one instance of ShardRecordProcessor which handles
    records from the stream shard and one instance of Scheduler which
    uses the ShardRecordProcessorFactory implementation to find the
    appropiate ShardRecordProcessor instance to handle one specific type of
    record received from the stream shard.

---
+++ src/main/java/com/example/streaming/EnrollmentStreamConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.streaming;
+
+import com.example.event.EventConsumerFactory;
+import com.example.streaming.configuration.StreamProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import software.amazon.kinesis.coordinator.Scheduler;
+import software.amazon.kinesis.processor.ShardRecordProcessor;
+
+@Configuration
+public class EnrollmentStreamConfiguration {
+
+    private static final String STREAM_ID = "EnrollmentStream";
+    public static final String STREAM_PROPERTIES = STREAM_ID + "Properties";
+    private static final String STREAM_PROCESSOR = STREAM_ID + "Processor";
+    private static final String STREAM_SCHEDULER = STREAM_ID + "Scheduler";
+
+    @Bean(STREAM_PROPERTIES)
+    StreamProperties properties(Environment environment) {
+        return new StreamProperties(
+            new StreamProperties.Id(STREAM_ID),
+            new StreamProperties.Stream(environment.getRequiredProperty("streams.enrollment.name", String.class)),
+            new StreamProperties.Application(environment.getRequiredProperty("streams.enrollment.application", String.class))
+        );
+    }
+
+    @Bean(STREAM_SCHEDULER)
+    Scheduler scheduler(ApplicationContext context, @Qualifier(STREAM_PROPERTIES) StreamProperties properties) {
+        return new EventStreamScheduler(context, properties);
+    }
+
+    @Bean(STREAM_PROCESSOR)
+    ShardRecordProcessor processor(EventConsumerFactory eventConsumerFactory, ObjectMapper objectMapper) {
+        return new EventStreamProcessor(eventConsumerFactory, objectMapper);
+    }
+}
